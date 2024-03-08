787b4b51b5024092bef9231a532f62c0f85349a3
Decapix <decapixd@gmail.com>
2024-03-08 10:23:11 +0100

    small update

--- src/mevsite/settings.py
+++ src/mevsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y4vjw%qk!*432)fzs-q6lj26xnn+t1e5)d(2*!!*lm=%_ooid^"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -179,7 +179,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'mev.message@gmail.com'
 EMAIL_HOST_USER = 'mev.message@gmail.com'
-EMAIL_HOST_PASSWORD = 'luakgdmfxlakmbml'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 

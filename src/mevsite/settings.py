0e44777cc2cd3c1fb9ec2eb824fd550d27bd7af9
Decapix <decapixd@gmail.com>
2024-09-30 12:19:38 +0200

    try2

--- src/mevsite/settings.py
+++ src/mevsite/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['be-mev-e942436feae3.herokuapp.com', '127.0.0.1', 'be-mev.herokuapp.com', 'be-mev.com', 'www.be-mev.com', 'be-mev.fr', 'www.be-mev.fr']
